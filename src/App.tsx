1b6f83850b8a87450dfaab4ffbb81667ee4b2090
satenchauhan <satenchauhan@gmail.com>
2024-07-05 18:20:05 +0530

    Created Enviroment file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-
+  console.log(import.meta.env.VITE_CHRM_API_KEY);
   return (
     <>
       <h2>Vite + React + Storybook + Chronatic</h2>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    APP_KEY: JSON.stringify(process.env.API_KEY),
+  },
+});
